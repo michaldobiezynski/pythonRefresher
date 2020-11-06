b64cc633635429c15e6d37f3d21203286af330cc
michaldobiezynski <michaldobiezynski@hotmail.com>
2020-11-06 14:06:21 +0000

    if statements and comments

--- pythonRefresher.py
+++ pythonRefresher.py
@@ -1,4 +1,11 @@
-age = 26
+age = 12
 name = "Michal"
 
-print("Hello my name is {} and I am {} years old".format(name, age))
+# print("Hello my name is {} and I am {} years old".format(name, age))
+
+todayIsCold = True
+
+if age > 18:
+    print("You are older than 18")
+else:
+    print("You are younger than 18")
