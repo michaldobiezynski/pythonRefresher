919327424617f1d31e56578666a5da180ddc6f40
michaldobiezynski <michaldobiezynski@hotmail.com>
2020-11-06 14:19:38 +0000

    functions

--- pythonRefresher.py
+++ pythonRefresher.py
@@ -3,9 +3,18 @@ name = "Michal"
 
 # print("Hello my name is {} and I am {} years old".format(name, age))
 
-todayIsCold = True
+# todayIsCold = True
 
-if age > 18:
-    print("You are older than 18")
-else:
-    print("You are younger than 18")
+# if age > 18:
+#     print("You are older than 18")
+# else:
+#     print("You are younger than 18")
+
+
+def hello(name="Sean", age=0):
+    return "Hello {} you are {}".format(name, age)
+
+
+sentence = hello("Michal", 17)
+print(sentence)
+print(hello())
